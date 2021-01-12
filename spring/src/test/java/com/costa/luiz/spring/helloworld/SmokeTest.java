306776e1b5df2aec080ee3a8e1b99239014b0cd3
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2021-01-12 22:52:55 +0100

    Add Spring tests

---
+++ spring/src/test/java/com/costa/luiz/spring/helloworld/SmokeTest.java
@@ -0,0 +1,19 @@
+package com.costa.luiz.spring.helloworld;
+
+import com.costa.luiz.spring.helloworld.resource.HelloWorldController;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SmokeTest implements WithAssertions {
+
+    @Autowired
+    private HelloWorldController controller;
+
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+}
---
+++ spring/src/test/java/com/costa/luiz/spring/helloworld/resource/HelloWorldControllerTest.java
@@ -0,0 +1,25 @@
+package com.costa.luiz.spring.helloworld.resource;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloWorldControllerTest implements WithAssertions {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/spring/helloworld/luiz",
+                String.class)).contains("Spring saying 'Hello to");
+    }
+
+}
